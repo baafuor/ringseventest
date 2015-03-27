4806ae0978d219a50bb55ed5f01825af3a0f13c3
Barima Kwarteng <baafuorkwarteng@gmail.com>
2015-03-26 22:32:22 -0600

    Initial Commit

---
+++ ringsevencomptest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ringsevencomptest
+//
+//  Created by Baafuor Kwarteng on 3/26/15.
+//  Copyright (c) 2015 Baafuor Kwarteng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ringsevencomptestTests/ringsevencomptestTests.m
@@ -0,0 +1,40 @@
+//
+//  ringsevencomptestTests.m
+//  ringsevencomptestTests
+//
+//  Created by Baafuor Kwarteng on 3/26/15.
+//  Copyright (c) 2015 Baafuor Kwarteng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ringsevencomptestTests : XCTestCase
+
+@end
+
+@implementation ringsevencomptestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
